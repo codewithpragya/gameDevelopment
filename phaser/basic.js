aa8507880c23d0528919bf9976ea3bd06bc1ac62
Pragya Sharma <officialpragyasharma0106@gmail.com>
2021-05-11 03:53:44 +0530

    GameDevelopment

---
+++ phaser/basic.js
@@ -0,0 +1,17 @@
+const x = 2;
+console.log(x);
+
+function demo(){
+    console.log("In Function Demo")
+}
+demo();
+
+if(true){
+    console.log("In If block")
+}
+else if(true){
+    console.log("Else if")
+}
+else{
+    console.log("Else condition")
+}
